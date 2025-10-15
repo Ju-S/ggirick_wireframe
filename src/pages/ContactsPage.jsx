114153222a382ecae1b12f1e1eaccbdddb251ce8
gijun123 <tteenn1@naver.com>
2025-10-15 16:55:55 +0900

    ㅈ..ㅈ.주주..주주소록ㅇ..이이지롱~~~!

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -3,9 +3,9 @@ import Contacts from "../commons/components/contacts/Contacts.jsx";
 
 export default function ContactsPage() {
   return (
-    <div className="flex h-screen bg-gray-50 p-4 pt-20 md:ml-64">
-      {/*주소록 */}
-      <Contacts />
-    </div>
+    <>{/*주소록 */}
+      <Contacts /></>
+
+
   );
 }
